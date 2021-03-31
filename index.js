2d3ab4ba78c7e44c1bdd3f90b8b0443294c7bc5b
Jennifer Pazos <jmp521@gmail.com>
2021-03-31 16:57:49 -0500

    commit tree 342d888218266db47fa2978ac80b70499e783370

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+let dodger = document.getElementById("dodger")
+console.log(dodger)
+function moveDodgerLeft() {
+    dodger.style.left = 0
+}
+
+function moveDodgerRight() {
+    dodger.style.left = "360px"
+}
